e630bc963b8992769b7947811687147f15567156
Quoc Phong Tran <tran0524@algonquinlive.com>
2025-03-11 17:30:02 -0400

    week9

--- lib/database.dart
+++ lib/database.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
-import 'todo_dao.dart';
 import 'package:floor/floor.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
 import 'todo_item.dart';
+import 'todo_dao.dart';
+
 part 'database.g.dart';
 
-@Database(version: 1, entities: [TodoItem])
+@Database(version: 1, entities: [ToDoItem])
 abstract class AppDatabase extends FloorDatabase {
   TodoDao get todoDao;
 }
--- lib/todo_dao.dart
+++ lib/todo_dao.dart
@@ -1,13 +1,14 @@
 import 'package:floor/floor.dart';
-import 'package:flutter/material.dart';
 import 'todo_item.dart';
 
 @dao
 abstract class TodoDao {
-  @Query("Select * from TodoItem")
-  Future<List<TodoItem>> getAllItems();
+  @Query('SELECT * FROM ToDoItem')
+  Future<List<ToDoItem>> getAllItems();
+
   @insert
-  Future<void> insertItems(TodoItem itm);
+  Future<void> insertItem(ToDoItem item);
+
   @delete
-  Future<void> deleteItems(TodoItem itm);
+  Future<void> deleteItem(ToDoItem item);
 }
